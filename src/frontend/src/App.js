043bca4717761cd7bec59a7aaf5402b8dd537f45
billywba <billy.wba8@gmail.com>
2022-06-01 23:47:19 +0100

    router: added not found page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom"
 import Homepage from './pages/homepage/Homepage';
 import Room from './pages/room/Room';
+import NotFound from './pages/notfound/NotFound';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/room" element={<Room />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </header>
       </div>
---
+++ src/frontend/src/pages/notfound/NotFound.js
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <>
+      <h1>NOT FOUND</h1>
+    </>
+  );
+}
+
+export default NotFound;
