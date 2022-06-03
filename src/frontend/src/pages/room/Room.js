4a1ec97c008fd8f2656c0e632defdc889f00d30c
billywba <billy.wba8@gmail.com>
2022-06-03 15:52:04 +0100

    room-video: added input box for video url

--- src/frontend/src/pages/room/Room.js
+++ src/frontend/src/pages/room/Room.js
@@ -1,9 +1,13 @@
 import ReactPlayer from 'react-player/youtube'
+import VideoSearchInput from '../video/VideoSearch';
 
 function Room() {
     return (
         <>
             {/* <h1>ROOM</h1> */}
+
+            <VideoSearchInput />
+
             <ReactPlayer 
                 url='https://www.youtube.com/watch?v=fizcJUBCx5Y-U' 
                 width='1280px' 
---
+++ src/frontend/src/pages/video/VideoSearch.js
@@ -0,0 +1,9 @@
+function VideoSearchInput() {
+    return (
+        <>
+            <input placeholder="Enter YouTube URL" />
+        </>
+    );
+}
+
+export default VideoSearchInput;
