6ac8149113e495b2ef123c1461e1f1bfb470dd1a
billywba <billy.wba8@gmail.com>
2022-06-03 16:00:04 +0100

    room-video: useState for video player source

--- src/frontend/src/pages/room/Room.js
+++ src/frontend/src/pages/room/Room.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import ReactPlayer from 'react-player/youtube'
 import VideoSearchInput from '../video/VideoSearch';
 
 function Room() {
+
+    const [currentVideoURL, setCurrentVideoURL] = useState('https://www.youtube.com/watch?v=fizcJUBCx5Y-U' );
+
     return (
         <>
             {/* <h1>ROOM</h1> */}
@@ -9,7 +13,7 @@ function Room() {
             <VideoSearchInput />
 
             <ReactPlayer 
-                url='https://www.youtube.com/watch?v=fizcJUBCx5Y-U' 
+                url={currentVideoURL} 
                 width='1280px' 
                 height='720px'
                 controls='1'
