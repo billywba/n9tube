e98f5b1a8ab607408ef1636a94b0a096ae75e41b
billywba <billy.wba8@gmail.com>
2022-06-03 19:32:46 +0100

    room-video: prettier format

--- src/frontend/src/components/video/VideoSearch.js
+++ src/frontend/src/components/video/VideoSearch.js
@@ -1,24 +1,26 @@
 import { useState } from "react";
 
 function VideoSearchInput({ setCurrentVideoURL }) {
-    
-    const [videoSearchInputText, setVideoSearchInputText] = useState('');
+  const [videoSearchInputText, setVideoSearchInputText] = useState("");
 
-    const handleWatchButtonOnClick = () => {
-        console.log(`Loading ${videoSearchInputText}`);
-        setCurrentVideoURL(videoSearchInputText);
-    }
+  const handleWatchButtonOnClick = () => {
+    console.log(`Loading ${videoSearchInputText}`);
+    setCurrentVideoURL(videoSearchInputText);
+  };
 
-    const handleVideoSearchInputChange = (event) => {
-        setVideoSearchInputText(event.target.value);
-    }
+  const handleVideoSearchInputChange = (event) => {
+    setVideoSearchInputText(event.target.value);
+  };
 
-    return (
-        <>
-            <input onChange={handleVideoSearchInputChange} placeholder="Enter YouTube URL" />
-            <button onClick={handleWatchButtonOnClick}>Watch</button>
-        </>
-    );
+  return (
+    <>
+      <input
+        onChange={handleVideoSearchInputChange}
+        placeholder="Enter YouTube URL"
+      />
+      <button onClick={handleWatchButtonOnClick}>Watch</button>
+    </>
+  );
 }
 
 export default VideoSearchInput;
--- src/frontend/src/pages/room/Room.js
+++ src/frontend/src/pages/room/Room.js
@@ -1,25 +1,26 @@
-import { useState } from 'react';
-import ReactPlayer from 'react-player/youtube'
-import VideoSearchInput from '../../components/video/VideoSearch';
+import { useState } from "react";
+import ReactPlayer from "react-player/youtube";
+import VideoSearchInput from "../../components/video/VideoSearch";
 
 function Room() {
+  const [currentVideoURL, setCurrentVideoURL] = useState(
+    "https://www.youtube.com/watch?v=fizcJUBCx5Y-U"
+  );
 
-    const [currentVideoURL, setCurrentVideoURL] = useState('https://www.youtube.com/watch?v=fizcJUBCx5Y-U');
+  return (
+    <>
+      {/* <h1>ROOM</h1> */}
 
-    return (
-        <>
-            {/* <h1>ROOM</h1> */}
+      <VideoSearchInput setCurrentVideoURL={setCurrentVideoURL} />
 
-            <VideoSearchInput setCurrentVideoURL={setCurrentVideoURL}/>
-
-            <ReactPlayer 
-                url={currentVideoURL} 
-                width='1280px' 
-                height='720px'
-                controls={true}
-            />
-        </>
-    );
+      <ReactPlayer
+        url={currentVideoURL}
+        width="1280px"
+        height="720px"
+        controls={true}
+      />
+    </>
+  );
 }
 
 export default Room;
