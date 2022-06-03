0f21a232469aa0082cd138352ac330e817e01d4e
billywba <billy.wba8@gmail.com>
2022-06-03 13:44:12 +0100

    room-video: added basic ReactPlayer

--- src/frontend/src/pages/room/Room.js
+++ src/frontend/src/pages/room/Room.js
@@ -1,10 +1,17 @@
+import ReactPlayer from 'react-player/youtube'
+
 function Room() {
     return (
         <>
-            <h1>ROOM</h1>
+            {/* <h1>ROOM</h1> */}
+            <ReactPlayer 
+                url='https://www.youtube.com/watch?v=fizcJUBCx5Y-U' 
+                width='1280px' 
+                height='720px'
+                controls='1'
+            />
         </>
     );
-  }
-  
-  export default Room;
-  
+}
+
+export default Room;
