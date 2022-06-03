30ddd97d8285260762365e580ecc0a7a0b4c23fe
billywba <billy.wba8@gmail.com>
2022-06-03 19:40:32 +0100

    room-video: comments

--- src/frontend/src/components/video/VideoSearch.js
+++ src/frontend/src/components/video/VideoSearch.js
@@ -1,13 +1,27 @@
 import { useState } from "react";
 
+/**
+ * Provides input field and button for user to update the current video playing
+ * @param {function} setCurrentVideoURL - function to update the video player URL prop
+ */
 function VideoSearchInput({ setCurrentVideoURL }) {
+  /**
+   * Holds the current text in the search input element
+   */
   const [videoSearchInputText, setVideoSearchInputText] = useState("");
 
+  /**
+   * When watch is clicked, set the video player to the URL
+   */
   const handleWatchButtonOnClick = () => {
     console.log(`Loading ${videoSearchInputText}`);
     setCurrentVideoURL(videoSearchInputText);
   };
 
+  /**
+   * Update state which holds value of the input element
+   * @param {*} event
+   */
   const handleVideoSearchInputChange = (event) => {
     setVideoSearchInputText(event.target.value);
   };
