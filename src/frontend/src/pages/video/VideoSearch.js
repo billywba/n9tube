3534679fd2c4f9229699ae107d1ac63c2fdfdc38
billywba <billy.wba8@gmail.com>
2022-06-03 16:32:48 +0100

    room-video: added watch button and functionality

--- src/frontend/src/pages/video/VideoSearch.js
+++ src/frontend/src/pages/video/VideoSearch.js
@@ -1,7 +1,21 @@
-function VideoSearchInput() {
+import { useState } from "react";
+
+function VideoSearchInput(setCurrentVideoURL) {
+    
+    const [videoSearchInputText, setVideoSearchInputText] = useState('');
+
+    const handleWatchButtonOnClick = () => {
+        console.log(`Loading ${videoSearchInputText}`);
+    }
+
+    const handleVideoSearchInputChange = (event) => {
+        setVideoSearchInputText(event.target.value);
+    }
+
     return (
         <>
-            <input placeholder="Enter YouTube URL" />
+            <input onChange={handleVideoSearchInputChange} placeholder="Enter YouTube URL" />
+            <button onClick={handleWatchButtonOnClick}>Watch</button>
         </>
     );
 }
