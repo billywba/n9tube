f6a00d35bad2202b4f91a2c2592a9df340dc6a07
Yad <yadkarim99@gmail.com>
2022-06-04 16:46:54 +0100

    created navbar

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import Room from "./pages/room/Room";
 import NotFound from "./pages/notfound/NotFound";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <header className="bg-slate-1000" /> 
+      <header className="bg-slate-1000">
+        <Navbar />  
+      </header> 
        <div className="App bg-zinc-900">
           <Routes>
             <Route path="/" element={<Homepage />} />
