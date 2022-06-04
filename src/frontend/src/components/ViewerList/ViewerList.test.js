1caa6bcf9bc42490c7938770e7bb12132840b1f1
billywba <billy.wba8@gmail.com>
2022-06-04 19:56:51 +0100

    room-viewer-list: tests

---
+++ src/frontend/src/components/ViewerList/ViewerList.test.js
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+
+import ViewerList from "./ViewerList";
+
+test("VideoSearch renders a list of viewer's usernames", () => {
+  const testViewerList = [
+    { username: "test_user_1" },
+    { username: "yadk le in csgo" },
+    { username: "tense1983" }
+  ];
+  const { getByText } = render(<ViewerList viewerList={testViewerList} />);
+
+  expect(getByText(/test_user_1/)).toBeInTheDocument();
+  expect(getByText(/yadk le in csgo/)).toBeInTheDocument();
+});
+
+test("VideoSearch renders no other viewers when no users connected", () => {
+  const testViewerList = [];
+  const { getByText } = render(<ViewerList viewerList={testViewerList} />);
+
+  expect(getByText(/No other viewers/)).toBeInTheDocument();
+});
