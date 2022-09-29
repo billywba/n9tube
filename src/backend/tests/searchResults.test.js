3209973c95d143ce54c5c14e02bf80996d324845
SinghM21 <manis3r3@gmail.com>
2022-09-29 16:06:47 +0100

    searchResults test added

--- src/backend/tests/searchResults.test.js
+++ src/backend/tests/searchResults.test.js
@@ -1,8 +1,8 @@
 const assert = require('chai').assert;
 const getSearch = require("../searchResults").getSearch;
 
-describe('search function', () => {
-    it('should return 5 results', () => {
+describe('GET /results', () => {
+    it('Should return 5 results', () => {
         const search = getSearch("tekken 7")
         console.log(search)
         assert.equal(search.length, 5);
